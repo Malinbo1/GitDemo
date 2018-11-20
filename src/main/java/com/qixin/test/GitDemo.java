81b7cc07ae61fd45c3d36fab297cf31b9203a630
malinbo_glb <malinbo_glb@email.com>
2018-11-20 18:08:16 +0800

    这是Demo1的提交

--- src/main/java/com/qixin/test/GitDemo.java
+++ src/main/java/com/qixin/test/GitDemo.java
@@ -3,8 +3,8 @@ package com.qixin.test;
 public class GitDemo {
 
 	public static void main(String[] args) {
-		System.out.println("GitDemo2");
-		System.out.println("GitDemo2");
+		System.out.println("GitDemo1");
+		System.out.println("GitDemo1");
 	}
 	
 	void gitTest() {
