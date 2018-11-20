bef3e2f6a9678d38c14660ca959f0b0c8f4c68b3
malinbo_glb <malinbo_glb@email.com>
2018-11-20 18:12:31 +0800

    这是Demo2的提交

--- src/main/java/com/qixin/test/GitDemo.java
+++ src/main/java/com/qixin/test/GitDemo.java
@@ -5,6 +5,8 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("GitDemo2");
 		System.out.println("GitDemo2");
+		System.out.println("GitDemo2");
+		System.out.println("GitDemo2");
 	}
 	
 	void gitTest() {
