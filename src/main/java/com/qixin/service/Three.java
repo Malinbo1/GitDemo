78a01c4037cb9f715e300f890f03196b980a2d80
zhang_yin_yin <zhang_yin_yin_6688@163.com>
2018-11-20 22:15:23 +0800

    5

--- src/main/java/com/qixin/service/Three.java
+++ src/main/java/com/qixin/service/Three.java
@@ -3,7 +3,6 @@ package com.qixin.service;
 public class Three {
 		public static void main(String[] args) {
 			System.out.println("张荫荫,第一次提交");
-			System.out.println("申达股份水电费地方");
-			System.out.println("一次风格水电费地方");
+
 		}
 }
